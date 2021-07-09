bd892c833a238c2d6bf7a44ec3ac841e0bc2088c
jaysonbackes <jayson.backes@discounttire.com>
2021-07-09 16:30:45 -0700

    none

--- create_db.py
+++ create_db.py
@@ -6,15 +6,23 @@ k=input("Please input a key: ")
 v=input("Plesse input a value: ") 
 
 
-data = {'pi4': 'spare bedroom office desk'}
+data = {'pi4': ['pi4','spare bedroom office desk'],'purple lamy':['purple lamy','spare bedroom office desk']}
 
 
 
 df = pd.DataFrame.from_dict(data, orient="index", columns=['item','location'])
 
-df
-df.reset_index()
+df.index.names = ['item']
+
+df.columns = ['year','none']
+
+
+df.columns()
+df = df.reset_index()
 
 data = {'row_1': [3, 2, 1, 0], 'row_2': ['a', 'b', 'c', 'd']}
 
-pd.DataFrame.from_dict(data, orient='index')
+df2 = pd.DataFrame.from_dict(data, orient='index',
+                       columns=['A', 'B', 'C', 'D'])
+
+
